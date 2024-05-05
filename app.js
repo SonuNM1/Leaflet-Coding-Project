8cc9231fd0544cbc2a07250c153ad0733aa0e407
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-05-06 01:40:30 +0530

    6th May

--- app.js
+++ app.js
@@ -21,7 +21,79 @@ const tiles = L.tileLayer(tileUrl, {attribution}) ;
 tiles.addTo(map) ; 
 
 
+// creating a circle layer with specified radius and color, and adding it to the map
 
 const circleLayer = L.circle([22.9074872, 79.07306671], {radius: 20000, color: 'coral'})
-
 circleLayer.addTo(map) ; 
+
+// creating a rectangle layer with specified bounds and adding it to the map
+
+const bounds = [[54.559322, -5.767822], [56.1210604, -3.021240]];
+const rectangle = L.rectangle(bounds) ;
+rectangle.addTo(map) ; 
+
+// creating a polygon layer with specified coordinates 
+
+const bermudaTriangle = [
+    [[25.774, -80.19],
+     [18.466, -66.118],
+     [32.321 , -64.757]]
+] ; 
+const polygon = L.polygon(bermudaTriangle) ; 
+polygon.addTo(map) ; 
+
+// creating a polyline layer
+
+const latlong = [
+    [45.51, -122.68],
+    [37.77, -122.43],
+    [34.04, -118.2]
+] ; 
+const polyline = L.polyline(latlong, {} ) ; 
+polygon.addTo(map) ; 
+
+// circle marker radius -> pexels 
+// circle radius -> meter
+
+// creating a circle marker layer with specified coordinates, radius
+
+const circleMarker = L.circleMarker([18.920675417289807, 72.82952788802635], {
+    color: 'coral',
+    radius: 100
+}) ;
+circleMarker.addTo(map) ;  
+
+// custom icon 
+
+const icon = L.icon({
+    iconUrl: 'pizzaman.png',
+    iconSize: [27, 22]
+})
+
+// adding marker with custom icon
+
+const marker1 = L.marker([18.920675417289807, 72.82952788802635], {
+    icon
+}) ;
+
+// popup to the map
+
+marker1.bindPopup(`<h2>Pizza outlet 1<h2/>`) ; 
+
+marker1.addTo(map) ;
+
+/* 
+const marker1 = L.marker([18.920675417289807, 72.82952788802635], {
+    icon
+}).addTo(map).bindPopup(`<h2>Pizza outlet 1<h2/>`) ;
+*/
+
+const marker2 = L.marker([28.539914829877652, 77.27116736919079], {
+    icon
+}) ;
+
+// tooltip to the map
+
+marker2.bindTooltip(`<h2>Pizza outlet 1<h2/>`) ; 
+
+marker2.addTo(map) ;  
